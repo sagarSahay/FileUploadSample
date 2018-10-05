daa557041af68cf4c4e3959e453c61bda5c62405
sagar_ph <sagar@parcelvision.com>
2018-10-05 11:16:38 +0100

    First commit

---
+++ Controllers/UploadFilesController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadSample.Controllers
+{
+    using System;
+
+    public class UploadFilesController : Controller
+    {
+        #region snippet1
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, filePath});
+        }
+        [HttpPost]
+        [Route("/carrier/{carrierId}/service/{serviceId}/accountNumber/{accountNumber}/effectiveFrom/{effectiveFrom}/fileName/{fileName}")]
+        public async Task<IActionResult> PostBinary(Guid carrierId, Guid serviceId, string accountNumber, string effectiveFrom, string fileName)
+        {
+            using (var sr = new StreamReader(Request.Body))
+            {
+                var body = await sr.ReadToEndAsync();
+                return Ok(body);
+            }
+        }
+        #endregion
+    }
+}
